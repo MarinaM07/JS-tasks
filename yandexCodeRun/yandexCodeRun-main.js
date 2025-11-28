56461336db7ad3cc86f7d97d9c9b4a1426235b58
Marina Moiseenko <moiseenko.marina07@gmail.com>
2025-11-28 15:14:36 +0300

    Добавляет задачи YandexCodeRun

--- yandexCodeRun/yandexCodeRun-main.js
+++ yandexCodeRun/yandexCodeRun-main.js
@@ -1,10 +1,14 @@
 import { merge } from "./mergeArr.js";
 import { scan } from "./dungeon.js";
 import { isRelativies } from "./isRelatives.js";
+import { makeTable } from "./internShip/makeTable.js";
+import { memeSpreadTime } from "./internShip/memeSpreadTime.js";
 
 
 window.myApp = {
     merge,
     scan,
     isRelativies,
+    makeTable,
+    memeSpreadTime
 }
